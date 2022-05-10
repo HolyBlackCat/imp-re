b349d5d4ccd1aa723b5d219eb0142c8bb687a8ef
HolyBlackCat <blckcat@inbox.ru>
2022-05-10 12:34:15 +0300

    Math: `vec::dir{4,8}` now have an optional length parameter.

--- gen/make_math.cpp
+++ gen/make_math.cpp
@@ -7,7 +7,7 @@
 #include <sstream>
 #include <type_traits>
 
-#define VERSION "3.4.7"
+#define VERSION "3.4.8"
 
 #pragma GCC diagnostic ignored "-Wpragmas" // Silence GCC warning about the next line disabling a warning that GCC doesn't have.
 #pragma GCC diagnostic ignored "-Wstring-plus-int" // Silence clang warning about `1+R"()"` paUern.
@@ -674,10 +674,10 @@ int main(int argc, char **argv)
                                 output("[[nodiscard]] constexpr vec rot90(int steps = 1) const {switch (steps & 3) {default: return *this; case 1: return {-y,x}; case 2: return -*this; case 3: return {y,-x};}}\n");
 
                                 // Return one of the 4 main directions, `vec2(1,0).rot90(index)`
-                                output("[[nodiscard]] static constexpr vec dir4(int index) {return vec(1,0).rot90(index);}\n");
+                                output("[[nodiscard]] static constexpr vec dir4(int index, type len = 1) {return vec(len,0).rot90(index);}\n");
 
                                 // Return one of the 8 main directions (including diagonals).
-                                output("[[nodiscard]] static constexpr vec dir8(int index) {vec array[8]{vec(1,0),vec(1,1),vec(0,1),vec(-1,1),vec(-1,0),vec(-1,-1),vec(0,-1),vec(1,-1)}; return array[index & 7];}\n");
+                                output("[[nodiscard]] static constexpr vec dir8(int index, type len = 1) {vec array[8]{vec(len,0),vec(len,len),vec(0,len),vec(-len,len),vec(-len,0),vec(-len,-len),vec(0,-len),vec(len,-len)}; return array[index & 7];}\n");
 
                                 // Get the 4-direction
                                 output("[[nodiscard]] constexpr int angle4() const {type s = sum(); type d = diff(); return d<0&&s>=0?1:x<0&&d<=0?2:y<0&&s<=0?3:0;} // Non-cardinal directions round to the closest one, diagnoals round backwards, (0,0) returns zero.\n");
