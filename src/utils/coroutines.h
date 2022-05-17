6da99bec8713d240d20cdd1a6d8461efac34a5cf
HolyBlackCat <blckcat@inbox.ru>
2022-05-18 01:02:42 +0400

    A saner coroutine class.

--- src/utils/coroutines.h
+++ src/utils/coroutines.h
@@ -4,6 +4,7 @@
 #include <coroutine>
 #include <cstddef>
 #include <memory>
+#include <optional>
 #include <utility>
 
 /* A minimalistic coroutine implementation.
@@ -32,12 +33,28 @@ class Coroutine
     template <typename, typename...>
     friend class std::coroutine_traits;
 
+    // Used below to conditionally disable some overloads.
+    struct NoType {};
+
     static constexpr auto ReturnTypeTraits() requires std::is_void_v<R>
     {
         struct Helper
         {
-            using ptr = bool;
-            using yield = std::nullptr_t;
+            using return_type_opt = bool; // Represents an optional `R`, returned when resuming the coroutine.
+            using return_storage = NoType; // Used internally to store the return value.
+            using yield = NoType; // The parameter of `co_yield`.
+            using move_yield = NoType; // The parameter of the moving overload of `co_yield`.
+        };
+        return Helper{};
+    }
+    static constexpr auto ReturnTypeTraits() requires std::is_reference_v<R>
+    {
+        struct Helper
+        {
+            using return_type_opt = std::remove_reference_t<R> *;
+            using return_storage = std::remove_reference_t<R> *;
+            using yield = R;
+            using move_yield = NoType;
         };
         return Helper{};
     }
@@ -45,8 +62,10 @@ class Coroutine
     {
         struct Helper
         {
-            using ptr = std::remove_reference_t<R> *;
-            using yield = R &&;
+            using return_type_opt = std::optional<R>;
+            using return_storage = std::optional<R> *;
+            using yield = const R &;
+            using move_yield = R &&;
         };
         return Helper{};
     }
@@ -55,7 +74,10 @@ class Coroutine
     {
         Coroutine *coro = nullptr;
 
-        typename decltype(ReturnTypeTraits())::ptr return_val{};
+        // If we return by value, this points to `std::optional<R>` where we should construct the return value.
+        // If we return by reference, we write the address of the reference here.
+        // Lastly, if we return void, this is a bool, where `true` indicates a successful return.
+        [[no_unique_address]] typename decltype(ReturnTypeTraits())::return_storage return_val{};
 
         constexpr std::suspend_always initial_suspend() noexcept {return {};}
         constexpr std::suspend_never final_suspend() noexcept
@@ -81,15 +103,24 @@ class Coroutine
         template <typename T> requires std::is_void_v<R>
         T &&await_transform(T &&value)
         {
-            return_val = true;
             return std::forward<T>(value);
         }
 
         using yield_type = typename decltype(ReturnTypeTraits())::yield;
+        using move_yield_type = typename decltype(ReturnTypeTraits())::move_yield;
 
-        std::suspend_always yield_value(yield_type ref) requires(!std::is_void_v<R>)
+        std::suspend_always yield_value(yield_type ref) requires(!std::is_same_v<yield_type, NoType> && (std::is_reference_v<R> || std::is_copy_constructible_v<R>))
         {
-            return_val = &ref;
+            if constexpr (std::is_reference_v<R>)
+                return_val = &ref;
+            else
+                return_val->emplace(ref);
+            return {};
+        }
+        std::suspend_always yield_value(move_yield_type ref) requires(!std::is_same_v<move_yield_type, NoType> && std::is_move_constructible_v<R>)
+        {
+            static_assert(!std::is_reference_v<R>, "Internal error.");
+            return_val->emplace(std::move(ref));
             return {};
         }
     };
@@ -106,8 +137,10 @@ class Coroutine
     }
 
   public:
+    // The return type as specified in the template argument.
     using return_type = R;
-    using return_type_opt = typename decltype(ReturnTypeTraits())::ptr;
+    // The return type of `operator()`.
+    using return_type_opt = typename decltype(ReturnTypeTraits())::return_type_opt;
 
     constexpr Coroutine() {}
 
@@ -137,11 +170,13 @@ class Coroutine
         return bool(promise);
     }
 
-    // The return type is `std::remove_reference_t<R> *`, except for void coroutines returns `bool`.
     // Performs a single coroutine step.
-    // If the coroutine finishes, returns null. Otherwise returns a pointer to the yielded value, or `true` for void coroutines.
+    // If the coroutine finishes, returns null. Otherwise returns the yielded value.
     // If the coroutine throws, this also throws and zeroes the coroutine.
     // For null coroutines always returns null.
+    // If the coroutine returns `void`, the return type is bool (true if the coroutine hasn't finished yet).
+    // If the coroutine returns a reference, the return type is a pointer to the same type.
+    // Otherwise, the return type is `std::optional<R>`.
     [[nodiscard]] constexpr return_type_opt operator()()
     {
         if (!promise)
@@ -149,11 +184,31 @@ class Coroutine
 
         try
         {
-            GetHandle().resume();
-            if (promise)
-                return promise->return_val;
+            if constexpr (std::is_void_v<R>)
+            {
+                GetHandle().resume();
+                if (promise)
+                    return true;
+            }
+            else if constexpr (std::is_reference_v<R>)
+            {
+                GetHandle().resume();
+                if (promise)
+                    return promise->return_val;
+            }
             else
-                return {}; // The coroutine just finished.
+            {
+                std::optional<R> ret;
+                promise->return_val = &ret;
+                GetHandle().resume();
+                if (promise)
+                {
+                    assert(ret.has_value());
+                    return ret;
+                }
+            }
+
+            return {}; // The coroutine is done.
         }
         catch (...)
         {
@@ -198,3 +253,79 @@ struct std::coroutine_traits<Coroutine<R>, P...>
 {
     using promise_type = typename Coroutine<R>::Promise;
 };
+
+
+/* Some test cases:
+Coroutine<> c1 = []() -> Coroutine<>
+{
+    std::cout << "1\n";
+    co_await std::suspend_always{};
+    std::cout << "2\n";
+    co_await std::suspend_always{};
+    std::cout << "3\n";
+    co_return;
+}();
+
+while (auto x = c1())
+    std::cout << "---\n";
+std::cout << '\n';
+
+
+Coroutine<int> c2 = []() -> Coroutine<int>
+{
+    int x;
+    std::cout << "1\n";
+    co_yield x = 10;
+    std::cout << "2\n";
+    co_yield 20;
+    std::cout << "3\n";
+    co_return;
+}();
+
+while (auto x = c2())
+    std::cout << "--- " << x.value() << '\n';
+std::cout << '\n';
+
+Coroutine<int &> c3 = []() -> Coroutine<int &>
+{
+    int x;
+    std::cout << "1\n";
+    co_yield x = 10;
+    std::cout << "2\n";
+    co_yield x = 20;
+    std::cout << "3\n";
+    co_return;
+}();
+
+while (auto x = c3())
+    std::cout << "--- " << *x << '\n';
+std::cout << '\n';
+
+Coroutine<int &&> c4 = []() -> Coroutine<int &&>
+{
+    std::cout << "1\n";
+    co_yield 10;
+    std::cout << "2\n";
+    co_yield 20;
+    std::cout << "3\n";
+    co_return;
+}();
+
+while (auto x = c4())
+    std::cout << "--- " << *x << '\n';
+std::cout << '\n';
+
+Coroutine<std::unique_ptr<int>> c5 = []() -> Coroutine<std::unique_ptr<int>>
+{
+    std::cout << "1\n";
+    co_yield std::make_unique<int>(10);
+    std::cout << "2\n";
+    co_yield std::make_unique<int>(20);
+    std::cout << "3\n";
+    co_return;
+}();
+
+while (auto x = c5())
+    std::cout << "--- " << **x << '\n';
+std::cout << '\n';
+*/
