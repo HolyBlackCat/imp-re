d841b4d3f04f727e81725af7d01918e4d0e3b1ff
HolyBlackCat <blckcat@inbox.ru>
2022-12-25 01:27:19 +0600

    The demo now uses entities.

--- examples/demo/state_world.cpp
+++ examples/demo/state_world.cpp
@@ -1,12 +1,44 @@
 #include "main.h"
 
+#include "entities.h"
+
+struct MouseCamera : Camera, Tickable
+{
+    void Tick() override
+    {
+        pos = mouse.pos();
+    }
+};
+
+struct TestEntity : Tickable, Renderable
+{
+    IMP_STANDALONE_COMPONENT(Game)
+
+    float angle = 0;
+
+    void Tick() override
+    {
+        ImGui::ShowDemoWindow();
+
+        angle += 0.01;
+
+        if (mouse.right.pressed())
+            audio.Play("test_sound"_sound, mouse.pos()); // Use `Audio::GlobalData::Sound()` instead of `_sound` for more customization.
+    }
+
+    void Render() const override
+    {
+        r.iquad(game.get<Camera>()->pos, "dummy"_image).center().rotate(angle);
+        r.iquad(game.get<Camera>()->pos.rect_size(32)).center().rotate(angle).color(mouse.left.down() ? fvec3(1,0.5,0) : fvec3(0,0.5,1));
+        r.itext(game.get<Camera>()->pos, Graphics::Text(Fonts::main, STR((audio.ActiveSources())))).color(fvec3(1));
+    }
+};
+
 namespace States
 {
     STRUCT( World EXTENDS StateBase )
     {
         MEMBERS()
-
-        float angle = 0;
 
         void Init() override
         {
@@ -15,17 +47,20 @@ namespace States
             Audio::ListenerPosition(fvec3(0, 0, -audio_distance));
             Audio::ListenerOrientation(fvec3(0,0,1), fvec3(0,-1,0));
             Audio::Source::DefaultRefDistance(audio_distance);
+
+            // Entities.
+            game = nullptr;
+
+            game.create<MouseCamera>();
+            game.create<TestEntity>();
         }
 
         void Tick(std::string &next_state) override
         {
             (void)next_state;
 
-            angle += 0.01;
-            ImGui::ShowDemoWindow();
-
-            if (mouse.right.pressed())
-                audio.Play("test_sound"_sound, mouse.pos()); // Use `Audio::GlobalData::Sound()` instead of `_sound` for more customization.
+            for (auto &e : game.get<AllTickable>())
+                e.get<Tickable>().Tick();
         }
 
         void Render() const override
@@ -35,9 +70,8 @@ namespace States
 
             r.BindShader();
 
-            r.iquad(mouse.pos(), "dummy"_image).center().rotate(angle);
-            r.iquad(mouse.pos().rect_size(32)).center().rotate(angle).color(mouse.left.down() ? fvec3(1,0.5,0) : fvec3(0,0.5,1));
-            r.itext(mouse.pos(), Graphics::Text(Fonts::main, STR((audio.ActiveSources())))).color(fvec3(1));
+            for (auto &e : game.get<AllRenderable>())
+                e.get<Renderable>().Render();
 
             r.Finish();
         }
