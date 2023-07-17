aa44c6e7184c4e657065ad2545b8268f44adfb28
HolyBlackCat <blckcat@inbox.ru>
2023-07-17 21:42:30 +0300

    Add `Storage::TemporaryPoolRef` and improve the monotonic pool.

--- src/utils/monotonic_pool.h
+++ src/utils/monotonic_pool.h
@@ -7,7 +7,9 @@
 #include <span>
 #include <type_traits>
 #include <vector>
+#include <utility>
 
+#include "program/errors.h"
 #include "utils/alignment.h"
 
 namespace Storage
@@ -27,17 +29,21 @@ namespace Storage
     // Can only remove all objects at once, but maintains some persistent memory even after that.
     class MonotonicPool
     {
-        // The current pool we're allocating from.
-        std::unique_ptr<std::uint8_t[]> pool;
-        // How much memory `pool` points to.
-        std::size_t pool_size = 0;
-        // The current offset into `pool`.
-        std::size_t pool_pos = 0;
+        struct State
+        {
+            // The current pool we're allocating from.
+            std::unique_ptr<std::uint8_t[]> pool;
+            // How much memory `pool` points to.
+            std::size_t pool_size = 0;
+            // The current offset into `pool`.
+            std::size_t pool_pos = 0;
 
-        // Older smaller pools. We don't allocate new objects in those, but keep them until the next `DestroyContents()`.
-        std::vector<std::unique_ptr<std::uint8_t[]>> old_pools;
-        // How much memory is currently sitting in `old_pools`.
-        std::size_t old_pools_combined_size = 0;
+            // Older smaller pools. We don't allocate new objects in those, but keep them until the next `DestroyContent()`.
+            std::vector<std::unique_ptr<std::uint8_t[]>> old_pools;
+            // How much memory is currently sitting in `old_pools`.
+            std::size_t old_pools_combined_size = 0;
+        };
+        State state{};
 
       public:
         // Creates an empty pool. You can still allocate objects in it, but it will start growing from a tiny size.
@@ -45,35 +51,48 @@ namespace Storage
 
         // Creates a pool of the specified size.
         explicit MonotonicPool(std::size_t initial_size)
-            : pool(std::make_unique_for_overwrite<std::uint8_t[]>(initial_size)), pool_size(initial_size)
-        {}
+        {
+            state.pool = std::make_unique_for_overwrite<std::uint8_t[]>(initial_size);
+            state.pool_size = initial_size;
+        }
 
-        MonotonicPool(MonotonicPool &&) = default;
-        MonotonicPool &operator=(MonotonicPool &&) = default;
+        MonotonicPool(MonotonicPool &&other) noexcept : state(std::exchange(other.state, {})) {}
+        MonotonicPool &operator=(MonotonicPool other) noexcept
+        {
+            std::swap(state, other.state);
+            return *this;
+        }
 
-        // The current persistent pool capacity that will be kept through `DestroyContents()` calls.
+        // Returns true if the pool has any objects allocated in it,
+        // or false if it's freshly constructed or had `DestroyContent()` just called on it.
+        [[nodiscard]] constexpr bool HasContent() const
+        {
+            return state.pool_pos > 0 || state.old_pools_combined_size > 0;
+        }
+
+        // The current persistent pool capacity that will be kept through `DestroyContent()` calls.
         // For true memory consumption, see `CurrentMemoryUsage()`.
         [[nodiscard]] constexpr std::size_t PermanentCapacity() const
         {
-            return pool_size;
+            return state.pool_size;
         }
 
         // The current amount of memory occupied with user objects.
         // There's a small amount of metadata that's not included here.
-        // Some of this memory will be freed on the next `DestroyContents()` call, but not all of it. See `PermanentCapacity()`.
+        // Some of this memory will be freed on the next `DestroyContent()` call, but not all of it. See `PermanentCapacity()`.
         [[nodiscard]] constexpr std::size_t CurrentMemoryUsage() const
         {
-            return pool_size + old_pools_combined_size;
+            return state.pool_size + state.old_pools_combined_size;
         }
 
         // Destroys all contents of the pool.
         // Keeps some memory for future allocations.
-        void DestroyContents()
+        void DestroyContent()
         {
-            pool_pos = 0;
-            old_pools_combined_size = 0;
-            old_pools.clear();
-            old_pools.shrink_to_fit();
+            state.pool_pos = 0;
+            state.old_pools_combined_size = 0;
+            state.old_pools.clear();
+            state.old_pools.shrink_to_fit();
         }
 
         // Allocate a block of raw memory.
@@ -84,29 +103,29 @@ namespace Storage
         requires MonotonicPoolSupportedAlignment<A>
         [[nodiscard]] std::uint8_t *AllocateRawMemory(std::size_t size, F &&func = nullptr)
         {
-            std::size_t needed_block_start = Align<A>(pool_pos);
+            std::size_t needed_block_start = Align<A>(state.pool_pos);
             std::size_t needed_block_end = needed_block_start + size;
-            if (needed_block_end > pool_size)
+            if (needed_block_end > state.pool_size)
             {
                 // Need a larger pool.
 
                 // Allocate the new pool.
-                std::size_t new_pool_size = std::max(size * 2, pool_size * 2);
+                std::size_t new_pool_size = std::max(size * 2, state.pool_size * 2);
                 auto new_pool = std::make_unique<std::uint8_t[]>(new_pool_size);
 
                 // Replace the current pool with the new one.
-                old_pools.push_back(std::move(pool)); // This line goes first because it can throw.
-                old_pools_combined_size += pool_size;
-                pool = std::move(new_pool);
-                pool_size = new_pool_size;
-                pool_pos = 0;
+                state.old_pools.push_back(std::move(state.pool)); // This line goes first because it can throw.
+                state.old_pools_combined_size += state.pool_size;
+                state.pool = std::move(new_pool);
+                state.pool_size = new_pool_size;
+                state.pool_pos = 0;
 
                 // Update the offsets.
                 needed_block_start = 0;
                 needed_block_end = size;
             }
 
-            const auto ret = pool.get() + needed_block_start;
+            const auto ret = state.pool.get() + needed_block_start;
 
             // Invoke the user callback.
             if constexpr (!std::is_null_pointer_v<F>)
@@ -116,7 +135,7 @@ namespace Storage
             }
 
             // Confirm the allocation.
-            pool_pos = needed_block_end;
+            state.pool_pos = needed_block_end;
             return ret;
         }
 
@@ -151,5 +170,39 @@ namespace Storage
             });
             return {ret, n};
         }
+    };
+
+    // Stores a pointer to a `MonotonicPool`.
+    // Expects to be given an empty pool, and clears it when destroyed.
+    class TemporaryPoolRef
+    {
+        MonotonicPool *pool = nullptr;
+
+      public:
+        constexpr TemporaryPoolRef() {}
+        constexpr TemporaryPoolRef(std::nullptr_t) {}
+        constexpr TemporaryPoolRef(MonotonicPool &pool) : TemporaryPoolRef(&pool) {}
+        constexpr TemporaryPoolRef(MonotonicPool *pool) : pool(pool)
+        {
+            ASSERT(!pool || !pool->HasContent(), "`TemporaryPoolRef` expects an empty pool.");
+        }
+
+        constexpr TemporaryPoolRef(TemporaryPoolRef &&other) noexcept : pool(std::exchange(other.pool, nullptr)) {}
+        TemporaryPoolRef &operator=(TemporaryPoolRef other) noexcept
+        {
+            std::swap(pool, other.pool);
+            return *this;
+        }
+
+        ~TemporaryPoolRef()
+        {
+            pool->DestroyContent();
+        }
+
+        [[nodiscard]] constexpr explicit operator bool() const noexcept {return bool(pool);}
+
+        // Intentionally doesn't propagate const.
+        [[nodiscard]] MonotonicPool &Pool() const noexcept {return *pool;}
+        [[nodiscard]] MonotonicPool *operator->() const noexcept {return pool;}
     };
 }
