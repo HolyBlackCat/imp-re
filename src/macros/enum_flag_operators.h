8aae2aa5d50a8fd498b6fb9452f12766c1a1c92e
HolyBlackCat <blckcat@inbox.ru>
2023-11-26 18:22:34 +0600

    `IMP_ENUM_FLAG_OPERATORS`: Generate enum * bool multiplication.

--- src/macros/enum_flag_operators.h
+++ src/macros/enum_flag_operators.h
@@ -4,7 +4,7 @@
 
 #include "program/compiler.h"
 
-// Synthesizes operators for a enum of flags: `&`, `|`, and `~`.
+// Synthesizes operators for a enum of flags: `&`, `|`, and `~`. Also multiplication by a bool.
 #define IMP_ENUM_FLAG_OPERATORS(name_) IMP_ENUM_FLAG_OPERATORS_CUSTOM(static, name_)
 
 // Same, but works at class scope.
@@ -16,4 +16,7 @@
     [[nodiscard, maybe_unused]] IMP_ALWAYS_INLINE prefix_ name_ operator|(name_ a, name_ b) {return name_(::std::underlying_type_t<name_>(a) | ::std::underlying_type_t<name_>(b));} \
     [[nodiscard, maybe_unused]] IMP_ALWAYS_INLINE prefix_ name_ operator~(name_ a) {return name_(~::std::underlying_type_t<name_>(a));} \
     [[maybe_unused]] IMP_ALWAYS_INLINE prefix_ name_ &operator&=(name_ &a, name_ b) {return a = a & b;} \
-    [[maybe_unused]] IMP_ALWAYS_INLINE prefix_ name_ &operator|=(name_ &a, name_ b) {return a = a | b;}
+    [[maybe_unused]] IMP_ALWAYS_INLINE prefix_ name_ &operator|=(name_ &a, name_ b) {return a = a | b;} \
+    [[nodiscard, maybe_unused]] IMP_ALWAYS_INLINE prefix_ name_ operator*(name_ a, bool b) {return b ? a : name_{};} \
+    [[nodiscard, maybe_unused]] IMP_ALWAYS_INLINE prefix_ name_ operator*(bool a, name_ b) {return a ? b : name_{};} \
+    [[maybe_unused]] IMP_ALWAYS_INLINE prefix_ name_ &operator*=(name_ &a, bool b) {return a = a * b;}
